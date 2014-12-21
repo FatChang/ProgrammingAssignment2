8f4e6011d5fae620268e16fd1f8cdcae9938eff9
changliufeng821@163.com <liufeng821>
2014-12-22 01:16:17 +0800

    revise

--- cachematrix.R
+++ cachematrix.R
@@ -7,8 +7,8 @@ makeCacheMatrix <- function(x = matrix()) {
   
   m<- NULL
   set <- function(y){
-    x<<y
-    m<<NULL
+    x<<-y
+    m<<-NULL
   }
   get <- function() x
   setsolve <- function(solve) m <<- solve
